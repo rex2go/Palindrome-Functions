963a646f1e48feae2a9e422e8d3e881f23f3db94
Tim Hübert <rexrex231@gmail.com>
2019-04-22 19:09:32 +0200

    php Palindrom-Funktion: Bereich zum Testen hinzugefügt

--- palindrome.php
+++ palindrome.php
@@ -4,4 +4,10 @@ function isPalindrome($word) {
     $reverse = strrev($word);
     return $word == $reverse;
 }
+
+/*
+echo("John: " . (isPalindrome("John") ? "Palindrom" : "kein Palindrom") . "<br>");
+echo("Hannah: " . (isPalindrome("Hannah") ? "Palindrom" : "kein Palindrom") . "<br>");
+echo("Kiwik: " . (isPalindrome("Kiwik") ? "Palindrom" : "kein Palindrom") . "<br>");
+*/
 ?>

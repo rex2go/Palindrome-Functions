4850ad267ea0d40b508a090d243878524bce16ec
Tim Hübert <rexrex231@gmail.com>
2019-04-22 19:11:01 +0200

    python Palindrom-Funktion: Bereich zum Testen hinzugefügt

--- palindrome.py
+++ palindrome.py
@@ -2,3 +2,12 @@ def is_palindrome(word):
     word = word.lower()
     reverse = word[::-1]
     return word == reverse
+
+'''
+inp = input("Wort eingeben: ")
+
+if is_palindrome(inp):
+    print("\"" + inp + "\" ist ein Palindrom")
+else:
+    print("\"" + inp + "\" ist kein Palindrom")
+'''
